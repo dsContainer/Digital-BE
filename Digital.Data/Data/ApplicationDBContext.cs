f283799cd6e3509e352463cb62521faa5b26b889
melloDr <thanhlong020920@gmail.com>
2022-11-08 12:13:04 +0700

    Add project files.

---
+++ Digital.Data/Data/ApplicationDBContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using Digital.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Digital.Data.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        #region
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<DocumentType> DocumentTypes { get; set; }
+        public DbSet<Signature> Signatures { get; set; }
+        #endregion
+    }
+}
+
---
+++ Digital.Data/Entities/Process.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital.Data.Entities
+{
+    public  class Process : BaseEntity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Template { get; set; }
+        public string? Status { get; set; }
+        public string? CompanyLevel { get; set; }
+        [ForeignKey("ProcessStepId")]
+        public string? ProcessStepId { get; set; }
+        public virtual ICollection<ProcessStep>? ProcessStep { get; set; } = new List<ProcessStep>();
+        [ForeignKey("BatchId")]
+        public string? BatchId { get; set; }
+        public virtual Batch? Batch { get; set; }
+
+    }
+}
